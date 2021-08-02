f6d8c8d32869bc652f15a78b5359b5c8777c4a82
Todd Weisse <taweisse@gmail.com>
2021-08-02 02:41:43 -0400

    byte writing functions implemeted

--- wav.js
+++ wav.js
@@ -1,6 +1,6 @@
 // WAV generation & playback library.
 
-'use strict'
+// 'use strict'
 
 /**
  * 
@@ -12,47 +12,67 @@
  *                              1 or 2 channels are supported.
  */
 WAV = (sampleRate, numChannels) => {
-    // Sanity check.
-    if (! Number.isInteger(sampleRate) || ! Number.isInteger(numChannels)) {
-        throw new Error('sample rate & channel count must be integers')
-    }
-    
-    // Assign sample rate.
-    if (sampleRate < 1 || sampleRate > 96000) {
-        throw new Error('unsupported sample rate')
-    } 
-    let _sampleRate = sampleRate
+	// Converts ASCII or integer data to an array of bytes. Integers are
+	// encoded with little endianess.
+	_writeBytes = (val, bytes) => {
+		data = new Uint8Array(bytes)
+		if (Number.isInteger(val)) {
+			for (let i = 0; i < bytes; i++) {
+				data[i] = (val >> (i * 8)) & 0xFF
+			}
+		} else {
+			for (let i = 0; i < bytes; i++) {
+				data[i] = val.charCodeAt(i)
+			}
+		}
+		return data
+	}
 
-    // Assign channel count.
-    if (numChannels < 1 || numChannels > 2) {
-        throw new Error('unsupported number of channels')
-    }
-    let _numChannels = numChannels
+	// Sanity check.
+	if (! Number.isInteger(sampleRate) || ! Number.isInteger(numChannels)) {
+		throw new Error('sample rate & channel count must be integers')
+	}
+	
+	// Assign sample rate.
+	if (sampleRate < 1 || sampleRate > 96000) {
+		throw new Error('unsupported sample rate')
+	} 
+	let _sampleRate = sampleRate
 
-    // Set up a WAV data buffer & a sample array for each channel.
-    let _wavData = new Uint8Array()
-    let _samples = []
-    for (let i = 0; i < this.numChannels; i++) {
-        _samples[i] = new Uint16Array()
-    }
+	// Assign channel count.
+	if (numChannels < 1 || numChannels > 2) {
+		throw new Error('unsupported number of channels')
+	}
+	let _numChannels = numChannels
 
-    // Member function definitions.
-    return ({
-        // Get the sample rate for this WAV object.
-        getSampleRate: () => {
-            return _sampleRate
-        },
+	// Write WAV header information.
+	let _wavData = new Uint8Array()
+	console.log(_writeACSII('RIFF'))
+	console.log(_writeInteger(12783, 4))
 
-        // Get the channel count for this WAV object.
-        getChannelCount: () => {
-            return _numChannels
-        },
+	// Set up a sample array for each channel.
+	let _samples = []
+	for (let i = 0; i < this.numChannels; i++) {
+		_samples[i] = new Uint16Array()
+	}
 
-        // 
-        addSamples: () => {
+	// Member function definitions.
+	return ({
+		// Get the sample rate for this WAV object.
+		getSampleRate: () => {
+			return _sampleRate
+		},
 
-        }
-    })
+		// Get the channel count for this WAV object.
+		getChannelCount: () => {
+			return _numChannels
+		},
+
+		// 
+		addSamples: () => {
+
+		}
+	})
 }
 
 // TESTING:
