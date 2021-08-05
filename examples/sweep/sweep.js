9c6bcc6bdf5fced97190a5d65c81213e6750a749
taweisse <taweisse@gmail.com>
2021-08-05 00:08:54 -0400

    moved sweep code to function and improved

--- examples/sweep/sweep.js
+++ examples/sweep/sweep.js
@@ -1,39 +1,47 @@
-// Generates a sweeping sin wave between 2 endpoint frequencies, and from left
+// Generates a sweeping sine wave between 2 endpoint frequencies, and from left
 // to right channel.
 
-// Setup.
-let freq1 = 1000
-let freq2 = 3000
-let secs = 5
-let sampleRate = 44100
+'use strict'
 
-// Calculate some things we'll need.
-let numSamples = sampleRate * secs
-let startInterval = (freq1 * 2 * Math.PI) / sampleRate
-let stopInterval = (freq2 * 2 * Math.PI) / sampleRate
-let intervalStep = (stopInterval - startInterval) / numSamples
-let strengthR = 0;
-let strengthL = 1;
-let panStep = 1 / numSamples
+function generateSweep(startFreq, endFreq, duration) {
+	const sampleRate = 44100
 
-// Generate samples for an ever-changing frequency.
-let sampL = new Float32Array(numSamples)
-let sampR = new Float32Array(numSamples)
-let curInterval = startInterval
-let curAngle = 0
-for (let i = 0; i < numSamples; i++) {
-	curSample = Math.sin(curAngle)
-	
-	sampL[i] = curSample * Math.pow(strengthL, 2)
-	sampR[i] = curSample * Math.pow(strengthR, 2)
-	
-	curAngle += curInterval
-	curInterval += intervalStep
-	strengthR += panStep
-	strengthL -= panStep
+	// Calculate some things we'll need.
+	let numSamples = sampleRate * duration
+	let startInterval = (startFreq * 2 * Math.PI) / sampleRate
+	let endInterval = (endFreq * 2 * Math.PI) / sampleRate
+	let intervalStep = (endInterval - startInterval) / numSamples
+	let pan = -1
+	let panStep = 2 / numSamples
+
+	// Generate samples at an ever-changing frequency.
+	let sampL = new Float32Array(numSamples)
+	let sampR = new Float32Array(numSamples)
+	let curInterval = startInterval
+	let curAngle = 0
+	for (let i = 0; i < numSamples; i++) {
+		// Generate a sample.
+		let curSample = Math.sin(curAngle)
+		
+		// Determine how much of the sample should go to the R & L channels.
+		let volL = Math.pow((1 - pan) / 2, 2)
+		let volR = Math.pow((pan + 1) / 2, 2)
+
+		// Add the panned sample to the arrays.
+		sampL[i] = curSample * volL
+		sampR[i] = curSample * volR
+
+		// Iterate.
+		curAngle += curInterval
+		curInterval += intervalStep
+		pan += panStep
+	}
+
+	// Generate the WAV.
+	let sweep = new WAV(sampleRate, 2)
+	sweep.addSamples([sampL, sampR])
+	return sweep;
 }
 
-// Generate the WAV.
-let sweep = new WAV(sampleRate, 2)
-sweep.addSamples([sampL, sampR])
+let sweep = generateSweep(1000, 3000, 5)
 sweep.download('sweep.wav')
