31e9b99073abc8bfb1eda567d9b09cfa0779b3c4
Todd Weisse <taweisse@gmail.com>
2023-08-02 18:32:53 -0400

    drawing works for waveform example

--- examples/waveform/waveform.js
+++ examples/waveform/waveform.js
@@ -2,12 +2,78 @@
 
 'use strict'
 
-// Generates a waveform based on what has been drawn on the canvas.
-function generateWaveform() {
+// Input validation.
+setInterval(() => {
+	if (freqInput.value.match(/^[0-9]+$/g)) {
+		freqInput.classList.remove('invalid')
+	} else {
+		freqInput.classList.add('invalid')
+	}
+}, 100)
+
+// ----------------------------------------------------------------------------
+// Waveform Generation.
+// ----------------------------------------------------------------------------
+
+// Reads the raw data from the canvas.
+function getCanvasData() {
 
 }
 
-// UI setup.
+// ----------------------------------------------------------------------------
+// Canvas Drawing.
+// ----------------------------------------------------------------------------
+
+const canvas = document.getElementById('waveformCanvas')
+const offset = canvas.getBoundingClientRect()
+const ctx = canvas.getContext('2d')
+ctx.lineCap = 'round'
+ctx.lineWidth = 10
+
+let lastMousePos = []
+function drawHandler(e) {
+	if (lastMousePos.length > 1) {
+		lastMousePos.shift()
+	}
+	lastMousePos.push({x: e.clientX - offset.left, y: e.clientY - offset.top})
+
+	if (lastMousePos.length > 1) {
+		ctx.beginPath()
+		ctx.moveTo(lastMousePos[0].x, lastMousePos[0].y)
+		ctx.lineTo(lastMousePos[1].x, lastMousePos[1].y)
+		ctx.stroke()
+	} else {
+		ctx.beginPath()
+		ctx.moveTo(lastMousePos[0].x, lastMousePos[0].y)
+		ctx.lineTo(lastMousePos[0].x + 0.1, lastMousePos[0].y + 0.1)
+		ctx.stroke()
+	}
+}
+
+canvas.addEventListener('mousedown', drawHandler)
+
+canvas.addEventListener('mousedown', () => {
+	canvas.addEventListener('mousemove', drawHandler)
+})
+
+canvas.addEventListener('mouseenter', () => {
+	lastMousePos = []
+})
+
+document.addEventListener('mouseup', () => {
+	canvas.removeEventListener('mousemove', drawHandler)
+	lastMousePos = []
+})
+
+const clearBtn = document.getElementById('clearBtn')
+clearBtn.addEventListener('click', () => {
+	ctx.clearRect(0, 0, canvas.width, canvas.height)
+})
+
+// ----------------------------------------------------------------------------
+// UI Setup
+// ----------------------------------------------------------------------------
+
 const waveformCanvas = document.getElementById('waveformCanvas')
 const freqInput = document.getElementById('freqInput')
 const playStopBtn = document.getElementById('playStopBtn')
